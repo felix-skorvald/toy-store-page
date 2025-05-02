c5a91f67718cef26667dca88a2ca5c7c6933ac5b
Felix Skörvald <felix@skorvald.se>
2025-05-02 23:13:04 +0200

    Routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createHashRouter, RouterProvider } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import Home from "./pages/Home.jsx";
+import Cart from "./pages/Cart.jsx";
+
+const router = createHashRouter([
+  {
+      path: "/",
+      Component: App,
+      children: [
+          { index: true, Component: Home },
+          { path: "/cart", Component: Cart },
+
+          
+      ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Toy Store</h1>
+        </div>
+    );
+};
+
+export default Home;
